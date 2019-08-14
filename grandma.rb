fff1429a4f2bee69e9f29f13651113421a99046c
James Mobus <mobusj@live.com>
2019-08-14 14:48:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,8 @@
 def speak_to_grandma(phrase)
   if(phrase == "I LOVE YOU GRANDMA!") then
     return "I LOVE YOU TOO PUMPKIN!"
-  end
   elsif(phrase == phrase.upper) then
     return "NO, NOT SINCE 1938!"
-  end
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
